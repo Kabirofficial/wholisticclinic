515863558bef7bbae3b59b6807cde6dd680c486e
Kabir <115813090+Kabirofficial@users.noreply.github.com>
2025-08-04 20:45:23 +0530

    v2 of website

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+
 import Navbar from './components/Navbar';
 import Preloader from './components/Preloader';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import Services from './pages/Services';
 import About from './pages/About';
@@ -14,13 +16,14 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 3000);
+    }, 3000); 
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <>
+      <ScrollToTop />
       {loading ? (
         <Preloader />
       ) : (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,47 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import "./Navbar.css";
-import logo from "../assets/images/logo.jpg"; 
-
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
+import logo from '../assets/images/logo.jpg';
+import { FaBars, FaTimes } from 'react-icons/fa';
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <NavLink to="/" className="navbar-logo">
-            <img src={logo} alt="Wholistic Clinic Logo" className="logo-img" />
-        </NavLink>
-        <ul className="nav-menu">
-          <li className="nav-item">
-            <NavLink to="/" className="nav-links">
-              Home
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/services" className="nav-links">
-              Services
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/about" className="nav-links">
-              About Us
-            </NavLink>
-          </li>
-          <li className="nav-item"> 
-            <NavLink to="/contact" className="nav-links-button">
-              Book Appointment
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
-};
+const [click, setClick] = useState(false);
+const handleClick = () => setClick(!click);
+const closeMobileMenu = () => setClick(false);
+return (
+<nav className="navbar">
+<div className="navbar-container">
+<NavLink to="/" className="navbar-logo" onClick={closeMobileMenu}>
+<img src={logo} alt="Wholistic Clinic Logo" className="logo-img" />
+</NavLink>
+    <div className="menu-icon" onClick={handleClick}>
+      {click ? <FaTimes /> : <FaBars />}
+    </div>
 
-export default Navbar;
+    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+      <li className="nav-item">
+        <NavLink to="/" className="nav-links" onClick={closeMobileMenu}>
+          Home
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink to="/services" className="nav-links" onClick={closeMobileMenu}>
+          Services
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink to="/about" className="nav-links" onClick={closeMobileMenu}>
+          About Us
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink to="/contact" className="nav-links-button" onClick={closeMobileMenu}>
+          Book Appointment
+        </NavLink>
+      </li>
+    </ul>
+  </div>
+</nav>
+);
+}
+
+export default Navbar
